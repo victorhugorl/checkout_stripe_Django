70797c8f07fe4ffe0791d98765b8a2fffebdd1eb
Victor <victorlimasg@outlook.com>
2024-10-04 19:48:45 -0300

    adicionando .gitignore e corrigindo erros

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('app.urls')),
-    path('payment/', include('payment.urls')),
+    path('', include('payment.urls')),
     path('admin/', admin.site.urls),
 ]
